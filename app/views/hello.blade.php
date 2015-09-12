963844dbe5f761a482cb56a1d3a74f8ca1b82724
Paul Kinch <pauljk1988@gmail.com>
2015-09-12 10:26:25 +0100

    Updated the master page and the home page with blade syntax

---
+++ app/views/hello.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('top-row')
+	<p>top row here</p>
+@stop
+
+@section('centre-row')
+	<p>centre row here</p>
+	<p>Grumpy wizards make toxic brew for the evil Queen and Jack.</p>
+@stop
+
+@section('bottom-row')
+	<p>bottom row here</p>
+@stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+<link rel="stylesheet" type="text/css" href="../css/base.css">
+<link rel="stylesheet" type="text/css" href="../css/layout.css">
+<script type="text/javascript" src="../js/min/expanded/scripts_header.min.js"></script>
+</head>
+<body>
+	@section('top-row')@show
+
+	@section('centre-row')@show
+
+	@section('bottom-row')@show
+
+<script type="text/javascript" src="../js/min/expanded/scripts_footer.min.js"></script>
+</body>
+</html>
