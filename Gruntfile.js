bc8dadb8eb841cc1e4bd26dde926596df1965eb4
Paul Kinch <pauljk1988@gmail.com>
2015-09-13 18:51:27 +0100

    Added the uglified js

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,7 +89,7 @@ grunt.initConfig({
 grunt.loadNpmTasks('grunt-contrib-compass');
 grunt.loadNpmTasks('grunt-contrib-sass');
 grunt.loadNpmTasks('grunt-contrib-concat');
-// grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-uglify');
 // Task definition
 grunt.registerTask('default', ['sass', 'concat']);
 };
