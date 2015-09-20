fa587eae445d523295dc4c8453ba0927903c0818
Paul Kinch <pauljk1988@gmail.com>
2015-09-20 16:54:54 +0100

    Updated the top nav to change active state on content shown unminified the JS

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -2,22 +2,19 @@
 
 @section('nav-row')
 	<div class="nav-row">
-		<div class="row">
-			<nav>
-				<ul class="top-nav large-12 large-centered column no-margin">
-					<div class="top-nav-left large-5 medium-12 small-12 columns no-padding">
-						<li class="large-6 small-6 columns">{{ HTML::link('#Biography', 'Biography')}}</li>
-						<li class="large-6 small-6 columns">{{ HTML::link('#Experience', 'Experience')}}</li>
-					</div>
-					<div class="top-nav-right large-7 medium-12 small-12 columns no-padding">
-						<li class="large-4 small-4 columns">{{ HTML::link('#Tools', 'Tools')}}</li>
-						<li class="large-4 small-4 columns">{{ HTML::link('#Projects', 'Projects')}}</li>
-						<li class="large-4 small-4 columns">{{ HTML::link('#Contact', 'Contact')}}</li>
-					</div>
-				</ul>
-
-			</nav>
-		</div>
+		<nav class="large-12 large-centered column">
+			<ul class="top-nav large-6 large-centered column no-margin">
+				<div class="top-nav-left large-5 medium-12 small-12 columns no-padding">
+					<li class="no-margin no-padding large-6 small-6 columns">{{ HTML::link('#Biography', 'Biography', array( 'class' => 'nav-item' ))}}</li>
+					<li class="no-margin no-padding large-6 small-6 columns">{{ HTML::link('#Experience', 'Experience', array( 'class' => 'nav-item' ))}}</li>
+				</div>
+				<div class="top-nav-right large-7 medium-12 small-12 columns no-padding">
+					<li class="no-margin no-padding large-4 small-4 columns">{{ HTML::link('#Tools', 'Tools', array( 'class' => 'nav-item' ))}}</li>
+					<li class="no-margin no-padding large-4 small-4 columns">{{ HTML::link('#Projects', 'Projects', array( 'class' => 'nav-item' ))}}</li>
+					<li class="no-margin no-padding large-4 small-4 columns">{{ HTML::link('#Contact', 'Contact', array( 'class' => 'nav-item' ))}}</li>
+				</div>
+			</ul>
+		</nav>
 	</div>
 	<div class="parallax-row">
 		<div class="parallax-background">
@@ -32,38 +29,38 @@
 @stop
 
 @section('bio-row')
-	<div class="bio-row" id="Biography">
+	<section class="bio-row" id="Biography">
 		<div class="row">
 		<h2>Bio</h2>
 			<p>Flank spare ribs capicola, strip steak biltong pancetta bresaola tri-tip cow landjaeger. Short ribs sirloin beef ribs, flank capicola ribeye turducken. Sirloin boudin andouille tail. Ham flank tail sausage t-bone, jerky landjaeger kevin porchetta ground round pork belly.</p>
 		</div>
-	</div>
+	</section>
 @stop
 
 @section('experience-row')
-	<div class="experience-row" id="Experience">
+	<section class="experience-row" id="Experience">
 		<div class="row">
 		<h2>Experience</h2>
 			<p>Bacon ipsum dolor sit amet chicken pork loin hamburger ball tip, swine tenderloin salami meatball boudin tri-tip fatback shank bacon. Pork salami kevin, brisket spare ribs strip steak ham hock porchetta. Pork belly short loin tail porchetta. Pork pig meatball, beef ribs doner jerky tri-tip hamburger filet mignon strip steak ball tip ham tail tongue corned beef.</p>
 			<p>Flank spare ribs capicola, strip steak biltong pancetta bresaola tri-tip cow landjaeger. Short ribs sirloin beef ribs, flank capicola ribeye turducken. Sirloin boudin andouille tail. Ham flank tail sausage t-bone, jerky landjaeger kevin porchetta ground round pork belly.</p>
 		</div>
-	</div>
-	<div class="tools-row" id="Tools">
+	</section>
+	<section class="tools-row" id="Tools">
 		<div class="row">	
 		<h2>Tools</h2>
 		<p>Flank spare ribs capicola, strip steak biltong pancetta bresaola tri-tip cow landjaeger. Short ribs sirloin beef ribs, flank capicola ribeye turducken. Sirloin boudin andouille tail. Ham flank tail sausage t-bone, jerky landjaeger kevin porchetta ground round pork belly.</p>
 		</div>
-	</div>
-	<div class="projects-row" id="Projects">
+	</section>
+	<section class="projects-row" id="Projects">
 		<div class="row">
 		<h2>Projects</h2>
 		<p>Bacon ipsum dolor sit amet chicken pork loin hamburger ball tip, swine tenderloin salami meatball boudin tri-tip fatback shank bacon. Pork salami kevin, brisket spare ribs strip steak ham hock porchetta. Pork belly short loin tail porchetta. Pork pig meatball, beef ribs doner jerky tri-tip hamburger filet mignon strip steak ball tip ham tail tongue corned beef.</p>
 		</div>
-	</div>
+	</section>
 @stop
 
 @section('contact-row')
-	<div class="contact-row" id="Contact">
+	<section class="contact-row" id="Contact">
 		<div class="row">
 			<h2>Contact</h2>
 			<div class="contact-container ">
@@ -90,6 +87,6 @@
 				<hr>
 			</div>
 		</div>
-	</div>
+	</section>
 	<div class="copyright">Site content Copyright &copy; 2015 Paul Kinch.</div>
 @stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,17 +5,21 @@
 	<title>Laravel PHP Framework</title>
 <link rel="stylesheet" type="text/css" href="../css/base.css">
 <link rel="stylesheet" type="text/css" href="../css/layout.css">
-<script type="text/javascript" src="../js/min/expanded/scripts_header.min.js"></script>
+<script type="text/javascript" src="../js/expanded/scripts_header.js"></script>
 </head>
 <body>
-	@section('nav-row')@show
 
+ 	@section('nav-row')@show
+
+<div class="sections">
 	@section('bio-row')@show
 
 	@section('experience-row')@show
 
-	@section('contact-row')@show
+	@section('contact-row')@show 
+</div>
 
-<script type="text/javascript" src="../js/min/expanded/scripts_footer.min.js"></script>
+
+<script type="text/javascript" src="../js/expanded/scripts_footer.js"></script>
 </body>
 </html>
